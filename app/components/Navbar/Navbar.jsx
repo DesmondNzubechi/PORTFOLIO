aba7617eb9974a79c2643a226ca346e83b440de8
Desmond Nzubechukwu Abugu <nzubestdesmond@gmail.com>
2023-09-29 13:02:45 +0100

    new change

--- app/components/Navbar/Navbar.jsx
+++ app/components/Navbar/Navbar.jsx
@@ -10,12 +10,12 @@ const Navbar = () => {
     const navs = ['about', 'projects', 'contact']
     const [showNav, setShowNav] = useState('right-[-1000px]')
     return (
-        <nav className='fixed w-full bg-slate-50 shadow-2xl flex flex-row justify-between py-[20px] px-[30px]'>
+        <nav className='fixed w-full bg-slate-50 overflow-x-hidden shadow-2xl flex flex-row justify-between py-[20px] px-[30px]'>
             <Link className='font-bold text-black uppercase text-[25px]' href='/'>Nzubechukwu</Link>
           
             <ul className={` font-fonty fixed ${showNav} w-[50%] md:w- top-0 justify-center md:static items-center bottom-0 md:px-0 md:py-0 py-[50px]  gap-[20px] px-[100px] flex flex-col md:flex-row  bg-slate-50 md:bg-0 rounded  shadow-xl md:shadow-none capitalize text-center `}>
        {navs.map(link => {
-                    return <li><Link className='text-[25px] ' href={link == 'home' ? '/' : link}>{ link}</Link></li>
+                    return <li><Link onClick={() => setShowNav('right-[-1000px]')} className='text-[25px] ' href={link == 'home' ? '/' : link}>{ link}</Link></li>
        })}
                 <hr />
                      <div className='flex md:hidden gap-3 items-center'>
@@ -26,8 +26,8 @@ const Navbar = () => {
                 </ul> 
                
             <div  className='relative z-10 flex md:hidden items-center '>
-              { showNav == 'right-[-1000px]' ? <AiOutlineMenu onClick={() => setShowNav('right-0')} className='text-[40px]' /> :
-                <FaXmark onClick={() => setShowNav('right-[-1000px]')} className='text-[40px]'/>}
+              { showNav == 'right-[-1000px]' ? <AiOutlineMenu onClick={() => setShowNav('right-0')} className='text-[40px] text-black hover:text-blue-500' /> :
+                <FaXmark onClick={() => setShowNav('right-[-1000px]')} className='text-[40px] text-black hover:text-red-500'/>}
          </div>
        </nav>
     )
