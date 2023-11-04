ce359e3e91b53ea650b67c066a884feafc02c0dd
Nzubechukwu Desmond Abugu <74661865+DesmondNzubechi@users.noreply.github.com>
2023-11-04 14:34:42 +0100

    Update Navbar.jsx

--- app/components/Navbar/Navbar.jsx
+++ app/components/Navbar/Navbar.jsx
@@ -11,20 +11,22 @@ const Navbar = () => {
     const pathname = usePathname(); 
     const navs = ['about', 'projects', 'contact']
   const [showNav, setShowNav] = useState({
-      duration: "duration-700",
+      duration: "duration-1000",
       display: "right-[-1000px]",
-      transition: "transition-all"
+      transition: "transition-all",
+      animate: "ease-out"
   })
     return ( 
         <nav className='fixed z-[100] w-full bg-slate-50 overflow-x-hidden shadow-2xl flex flex-row justify-between py-[20px] z-[100] px-[30px]'>
             <Link className='font-headerFont  font-bold text-black uppercase text-[25px]' href='/'>Nzubechukwu</Link>
           
-            <ul className={` font-fonty fixed ${showNav.display} w-[50%] md:w- top-0 justify-center  md:static items-center bottom-0 md:px-0 md:py-0 py-[50px]  gap-[20px] px-[100px] md:px-[20px] flex flex-col md:flex-row  bg-slate-50 md:bg-0 rounded  shadow-xl md:shadow-none capitalize text-center ${shownav.transition} ${showNav.duration} `}>
+            <ul className={` font-fonty fixed ${showNav.display} w-[50%] md:w- top-0 justify-center  md:static items-center bottom-0 md:px-0 md:py-0 py-[50px]  gap-[20px] px-[100px] md:px-[20px] flex flex-col md:flex-row  bg-slate-50 md:bg-0 rounded  shadow-xl md:shadow-none capitalize text-center ${shownav.transition} ${showNav.duration} ${showNav.animate} `}>
        {navs.map(link => {
                     return <li><Link onClick={() => setShowNav({
                         duration: "duration-700",
       display: "right-[-1000px]",
       transition: "transition-all" 
+                        animate: "ease-out"
                     })} className={`link ${pathname == `/${link}`? 'font-bold' : ''} text-slate-900 text-[25px] `} href={link == 'home' ? '/' : link}>{ link}</Link></li>
        })}
                 <hr />
@@ -37,14 +39,16 @@ const Navbar = () => {
                
             <div  className='relative z-10 flex md:hidden items-center '>
               { showNav.display == 'right-[-1000px]' ? <AiOutlineMenu onClick={() => setShowNav({
-                        duration: "duration-700",
+                        duration: "duration-1000",
       display: "right-0",
-      transition: "transition-all" 
+      transition: "transition-all",
+            animate: "ease-out"
                     })} className='text-[30px] text-black hover:text-blue-500' /> :
                 <FaXmark onClick={() => setShowNav({
-                        duration: "duration-700",
+                        duration: "duration-1000",
       display: "right-[-1000px]",
-      transition: "transition-all" 
+      transition: "transition-all" ,
+                    animate: "ease-out"
                     })} className='text-[30px] text-black hover:text-red-500'/>}
          </div>
        </nav>
