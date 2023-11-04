52f882e0e730858ddf844a6d2f5915abc1b29839
Desmond Nzubechukwu Abugu <nzubestdesmond@gmail.com>
2023-11-04 13:57:15 +0100

    new change

--- app/components/Navbar/Navbar.jsx
+++ app/components/Navbar/Navbar.jsx
@@ -12,10 +12,10 @@ const Navbar = () => {
     const navs = ['about', 'projects', 'contact']
   const [showNav, setShowNav] = useState('right-[-1000px]')
     return ( 
-        <nav className='fixed relative z-[100] w-full bg-slate-50 overflow-x-hidden shadow-2xl flex flex-row justify-between py-[20px] z-[100] px-[30px]'>
+        <nav className='fixed z-[100] w-full bg-slate-50 overflow-x-hidden shadow-2xl flex flex-row justify-between py-[20px] z-[100] px-[30px]'>
             <Link className='font-headerFont  font-bold text-black uppercase text-[25px]' href='/'>Nzubechukwu</Link>
           
-            <ul className={` font-fonty fixed ${showNav} w-[50%] md:w- top-0 justify-center  md:static items-center bottom-0 md:px-0 md:py-0 py-[50px]  gap-[20px] px-[100px] md:px-[20px] flex flex-col md:flex-row  bg-slate-50 md:bg-0 rounded  shadow-xl md:shadow-none capitalize text-center transition duration-1000 ease-out ... `}>
+            <ul className={` font-fonty fixed ${showNav} w-[50%] md:w- top-0 justify-center  md:static items-center bottom-0 md:px-0 md:py-0 py-[50px]  gap-[20px] px-[100px] md:px-[20px] flex flex-col md:flex-row  bg-slate-50 md:bg-0 rounded  shadow-xl md:shadow-none capitalize text-center transition duration-1000 ease-in ... `}>
        {navs.map(link => {
                     return <li><Link onClick={() => setShowNav('right-[-1000px]')} className={`link ${pathname == `/${link}`? 'font-bold' : ''} text-slate-900 text-[25px] `} href={link == 'home' ? '/' : link}>{ link}</Link></li>
        })}
