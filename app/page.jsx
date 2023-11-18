9300fc35df9a11dec4ac695f292570009b8318c7
Desmond Nzubechukwu Abugu <nzubestdesmond@gmail.com>
2023-11-18 06:44:49 +0100

    still making the site more accessible

--- app/page.jsx
+++ app/page.jsx
@@ -18,8 +18,8 @@ export default function Home() {
         <h1 className='font-bold text-black text-[15px] font-headerFont uppercase md:text-[40px] '>Desmond Nzubechukwu</h1>
         <p className='text-[13px] text-slate-500 md:text-[25px] font-[400]'>I am a dedicated <strong className='text-slate-900'>Frontend Developer</strong> with a primary focus on delivering value and innovative solutions to businesses, organizations, and communities through the application of my skills and expertise.</p>
         <div className='flex gap-5 items-center'>
-            <Link aria-label='link to my project' className='border p-2 rounded text-[15px] md:text-[20px] border-blue-500 text-slate-50 font-[500] hover:text-slate-50 hover:bg-slate-700 bg-slate-900' href='/projects'>My Projects</Link>
-            <Link aria-label='link to about me page' className='border p-2 rounded text-[15px] md:text-[20px] border-blue-500 text-slate-900 font-[500] hover:text-slate-50 hover:bg-slate-900' href='/about'>About Me</Link>
+            <Link aria-label='link to  projects page' className='border p-2 rounded text-[15px] md:text-[20px] border-blue-500 text-slate-50 font-[500] hover:text-slate-50 hover:bg-slate-700 bg-slate-900' href='/projects'>My Projects</Link>
+            <Link aria-label='link to about page' className='border p-2 rounded text-[15px] md:text-[20px] border-blue-500 text-slate-900 font-[500] hover:text-slate-50 hover:bg-slate-900' href='/about'>About Me</Link>
         </div>
       </div>
       <Image data-aos='fade-up' data-aos-duration='1000' className='bg-blue-500 transition shadow-2xl ease-in delay-100   rounded-full' alt='Nzubechukwu Desmond Abugu' src={myImage}></Image>
