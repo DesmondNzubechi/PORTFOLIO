255b12db0bb3882641f6edeaab59b0ef196d5a35
Desmond Nzubechukwu Abugu <nzubestdesmond@gmail.com>
2024-09-17 16:23:10 +0100

    changes

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -6,6 +6,9 @@ import { AiFillHtml5 } from 'react-icons/ai';
 import { BiLogoCss3, BiLogoJavascript, BiLogoFirebase, BiLogoTailwindCss } from 'react-icons/bi';
 import { FaReact, FaBootstrap } from 'react-icons/fa';
 import { SiTypescript } from "react-icons/si";
+import { FaNode } from "react-icons/fa";
+import { SiExpress } from "react-icons/si";
+import { DiMongodb } from "react-icons/di";
 
 const About = () => {
     return <div className="min-h-[100vh] bg-slate-50">
@@ -18,12 +21,12 @@ const About = () => {
   <div data-aos='fade-up' data-aos-duration='1000' className=" flex flex-col gap-5">
     <h1 className="text-slate-900 md:text-[30px] text-[25px] font-bold uppercase text-slate-900  font-headerFont">About Me</h1>
     <div className="text-slate-700 font-fonty max-w-[700px] flex flex-col gap-4 ">
-    <p className="md:text-[18px] text-slate-700   text-[15px] ">My name is <strong className="uppercase text-slate-900 font-bold">Desmond Nzubechukwu Abugu</strong>, and I am  a <strong className='capitalize'>frontend developer.</strong></p>
+    <p className="md:text-[18px] text-slate-700   text-[15px] ">My name is <strong className="uppercase text-slate-900 font-bold">Desmond Nzubechukwu Abugu</strong>, and I am  a <strong className='capitalize'>Software Engineer.</strong></p>
   {/* <p className="md:text-[18px] text-slate-700 text-[12px] ">In my journey as a web developer, I've led a collaborative effort to create an online learning platform  for Electrical Engineering students at my institution.  This platform provides students with easy access to their course outlines, downloadable learning resources, and a convenient CGPA calculator. It includes a dynamic blog that keeps students updated with the latest news and developments in the field of Electrical Engineering. Serving as the project manager, I oversaw the frontend development and ensured effective communication throughout the project's lifecycle.</p> */}
-              <p className="md:text-[17px] text-slate-700   text-[15px] ">In my journey as a web developer, I've led a collaborative effort to create an online learning platform for Electrical Engineering students at my institution.</p> 
+              <p className="md:text-[17px] text-slate-700   text-[15px] ">In my journey as a software engineer, I've led a collaborative effort to create an online learning platform for Electrical Engineering students at my institution.</p> 
               <p className="md:text-[17px] text-slate-700   text-[15px] ">This platform provides students with easy access to their course outlines, downloadable learning resources, and a convenient CGPA calculator.</p> 
               <p className="md:text-[17px] text-slate-700   text-[15px] ">It includes a dynamic blog that keeps students updated with the latest news and developments in the field of Electrical Engineering. </p> 
-              <p className="md:text-[17px] text-slate-700   text-[15px] ">Serving as the project manager, I oversaw the frontend development and ensured effective communication throughout the project's lifecycle.</p> 
+              <p className="md:text-[17px] text-slate-700   text-[15px] ">Serving as the project manager, I oversaw the the development of the whole project and ensured effective communication throughout the project's lifecycle.</p> 
               <p className="md:text-[17px] text-slate-700 text-[15px] ">In my career journey I have had the opportunity to collaborate with cross-functional teams, including designers, backend developers, and project managers, to deliver high-quality products on time and within budget. I am a strong communicator and collaborator, and enjoy working in fast-paced and dynamic environments.</p>
 
  <Link className='border p-2 rounded text-[15px] md:text-[20px] w-fit border-blue-500 text-slate-50 font-[500] hover:text-slate-50 hover:bg-slate-900 bg-blue-500' href='/contact'>Contact Me</Link>
@@ -48,6 +51,18 @@ const About = () => {
             <div className='flex flex-col items-center p-2 rounded-xl justify-center gap-1 shadow-2xl hover:bg-slate-200 cursor-pointer  '>
               <TbBrandNextjs className='text-[100px] text-blue-500 '/>
               <h2 className='text-slate-900  font-semibold text-[20px]'>NextJs</h2>
+            </div>
+            <div className='flex flex-col items-center p-2 rounded-xl justify-center gap-1 shadow-2xl hover:bg-slate-200 cursor-pointer  '>
+              <FaNode className='text-[100px] text-blue-500 '/>
+              <h2 className='text-slate-900  font-semibold text-[20px]'>NodeJs</h2>
+            </div>
+            <div className='flex flex-col items-center p-2 rounded-xl justify-center gap-1 shadow-2xl hover:bg-slate-200 cursor-pointer  '>
+              <SiExpress className='text-[100px] text-blue-500 '/>
+              <h2 className='text-slate-900  font-semibold text-[20px]'>ExpressJs</h2>
+            </div>
+            <div className='flex flex-col items-center p-2 rounded-xl justify-center gap-1 shadow-2xl hover:bg-slate-200 cursor-pointer  '>
+              <DiMongodb className='text-[100px] text-blue-500 '/>
+              <h2 className='text-slate-900  font-semibold text-[20px]'>mongoDb</h2>
             </div>
             <div className='flex flex-col items-center p-2 rounded-xl justify-center gap-1 shadow-2xl hover:bg-slate-200 cursor-pointer  '>
               <AiFillHtml5 className='text-[100px] text-blue-500 '/>
--- app/page.jsx
+++ app/page.jsx
@@ -16,7 +16,7 @@ export default function Home() {
     <main className="flex min-h-screen bg-white lg:flex-row  gap-5 bg-slate-50  flex-col py-[100px] pt-[150px] items-center font-poppins justify-around p-10">
       <div data-aos='fade-up' data-aos-duration='1000'  className='flex max-w-[700px] flex-col gap-5'>
         <h1 className='font-bold text-black text-[15px] font-headerFont uppercase md:text-[40px] '>Desmond Nzubechukwu</h1>
-        <p className='text-[13px] text-slate-500 md:text-[25px] font-[400]'>I am a dedicated <strong className='text-slate-900'>Frontend Developer</strong> with a primary focus on delivering value and innovative solutions to businesses, organizations, and communities through the application of my skills and expertise.</p>
+        <p className='text-[13px] text-slate-500 md:text-[25px] font-[400]'>I am a dedicated <strong className='text-slate-900'>Software Engineer</strong> with a primary focus on delivering value and innovative solutions to businesses, organizations, and communities through the application of my skills and expertise.</p>
         <div className='flex gap-5 items-center'>
             <Link aria-label='link to my projects page' className='border p-2 rounded text-[15px] md:text-[20px] border-blue-500 text-slate-50 font-[500] hover:text-slate-50 hover:bg-slate-700 bg-slate-900' href='/projects'>My Projects</Link>
             <Link aria-label='link to about me page' className='border p-2 rounded text-[15px] md:text-[20px] border-blue-500 text-slate-900 font-[500] hover:text-slate-50 hover:bg-slate-900' href='/about'>About Me</Link>
